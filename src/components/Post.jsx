2250a163696cf458128c8a5cbfbac3c42dabba41
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-11-24 10:54:12 -0300

    component avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post(props) {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img src="https://github.com/leolive1506.png" className={styles.avatar}  />
+                    <Avatar src="https://github.com/leolive1506.png" />
                     <div className={styles.authorInfo}>
                         <strong>Leonardo Lopes</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
-                <img src="https://github.com/leolive1506.png" className={styles.avatar}  />
+                <Avatar src="https://github.com/leolive1506.png" />
                 <strong>Leonardo Lopes</strong>
                 <span>Web Developer</span>
             </div>
