6d9201081cc59357b78275f3437b194f15c82cc7
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-11-24 09:51:43 -0300

    component post

---
+++ src/components/Post.jsx
@@ -0,0 +1,41 @@
+import styles from './Post.module.css'
+
+export function Post(props) {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img src="https://github.com/leolive1506.png" className={styles.avatar}  />
+                    <div className={styles.authorInfo}>
+                        <strong>Leonardo Lopes</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="25 de novembro às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder='Deixe seu comentário'
+                />
+
+                <footer>
+                    <button>Publicar</button>
+                </footer>
+            </form>
+        </article>
+    )
+} 
