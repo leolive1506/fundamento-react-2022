b4ed42f05f00bfed17133419949a76b4f64b812c
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-11-23 22:46:01 -0300

    initial sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
+
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Leonardo Lopes"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, maxime."
-      />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Leonardo Lopes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, maxime."
+          />
+          <Post
+            author="Leonardo Lopes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, maxime."
+          />
+          <Post
+            author="Leonardo Lopes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, maxime."
+          />
+        </main>
+      </div>
     </>
   )
 }
